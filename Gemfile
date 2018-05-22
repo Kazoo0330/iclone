8fff0e8502387746dc9d517b543272676e559734
kazoo <kazuki_saito@diveintocode.jp>
2018-05-22 10:47:15 +0900

    herokuでのletter_opener設定

--- Gemfile
+++ Gemfile
@@ -12,8 +12,9 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
-
+gem 'letter_opener_web'
 group :development, :test do
+
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'capybara', '~> 2.13'
   gem 'selenium-webdriver'
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   resources :posts
   resources :sessions, only: [:new, :create, :destroy]
   resources :favorites, only: [:create, :destroy, :index]
+  mount LetterOpenerWeb::Engine, at: "/letter_opener"
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
+  
 end
