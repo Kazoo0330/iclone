0efda07a551fa624d0203054eb00a255910cba31
kazoo <kazuki_saito@diveintocode.jp>
2018-05-24 20:14:44 +0900

    view_modified 1.0

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
   def destroy
     @user.destroy
     respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.html { redirect_to users_url, notice: 'ユーザー情報が削除されました。' }
       format.json { head :no_content }
     end
   end
