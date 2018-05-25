a2341e99648d9c4b1c7ce6ec392e4ae195d5b481
kazoo <kazuki_saito@diveintocode.jp>
2018-05-25 20:07:55 +0900

    view_modified 1.8

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   mount_uploader :image, ImageUploader
   validates :content, presence: true, length: { maximum:140 }
+  validates :title, presence: true, length: { maximum:65 }
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
   belongs_to :user
