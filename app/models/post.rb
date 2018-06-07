ae1081002affe52e9a4063b23ba14c5614e36629
kazoo <kazuki_saito@diveintocode.jp>
2018-06-07 17:03:22 +0900

    rubocop modified1.0

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   mount_uploader :image, ImageUploader
-  validates :content, presence: true, length: { maximum:140 }
-  validates :title, presence: true, length: { maximum:65 }
+  validates :content, presence: true, length: { maximum: 140 }
+  validates :title, presence: true, length: { maximum: 65 }
   validates :image, presence: true
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,12 @@ class User < ApplicationRecord
   mount_uploader :image, ImageUploader
   validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: true
-  
+
   before_save { email.downcase! }
-  
+
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
- 
+
   has_many :favorites, dependent: :destroy
   has_many :favorite_posts, through: :favorites, source: :post
   has_many :blogs
